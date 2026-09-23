474283b329e2cf9ced9e7533a90ffdfcc49dd364
Paola Moran <purplehoppe04@gmail.com>
2026-09-23 12:58:42 -0500

    Promedio de notas funcion

--- Program.cs
+++ Program.cs
@@ -5,15 +5,24 @@
 
     }    
 }
+//Funcion de promedio de notas
+static double PromedioNotas(int[] notasx)
+{
+    int suma=0;
+    for (int i=0;i<notasx.Length;i++)
+    {
+       suma +=notasx[i];
+    } 
+    return (double)suma/notasx.Length;
+}
+
 int [] notas={15,18,12,17,20};
+
 //Imprimir el arreglo de notas
 
 ImprimirNotas(notas);
 
 //Promedio de las notas
-int suma=0;
-for (int i=0;i<notas.Length;i++)
-{
-    suma +=notas[i];
-}
-Console.WriteLine($"Promedio de las notas:{(double)suma/notas.Length:F2}");
+double promedio = PromedioNotas(notas);
+
+Console.WriteLine($"Promedio de las notas:{promedio:F2}");
