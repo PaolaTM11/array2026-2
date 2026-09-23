2d2c072cc26ff4e30306e4246a0937741fc2722c
Paola Moran <purplehoppe04@gmail.com>
2026-09-23 12:52:16 -0500

    Promedio de notas

--- Program.cs
+++ Program.cs
@@ -5,5 +5,15 @@
 
     }    
 }
-int [] notas={5,18,12,17,20};
+int [] notas={15,18,12,17,20};
+//Imprimir el arreglo de notas
+
 ImprimirNotas(notas);
+
+//Promedio de las notas
+int suma=0;
+for (int i=0;i<notas.Length;i++)
+{
+    suma +=notas[i];
+}
+Console.WriteLine($"Promedio de las notas:{(double)suma/notas.Length:F2}");
