98fb597ca0fc8edee5793de93b9bc5308ff2c7de
Paola Moran <purplehoppe04@gmail.com>
2026-09-23 12:40:46 -0500

    Impresion de notas

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿static void ImprimirNotas(int [] notasx)
+{ for (int i=0;i<notasx.Length;i++)
+    {
+        System.Console.WriteLine($"Nota{i+1}:{notasx[i]}");
+
+    }    
+}
+int [] notas={5,18,12,17,20};
+ImprimirNotas(notas);
